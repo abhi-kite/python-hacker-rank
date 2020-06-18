cb058c92f5b4cf6900ab68ce23c82fe1c943c60d
abhi-kite <60897505+abhi-kite@users.noreply.github.com>
2020-06-18 09:39:39 +0530

    Create Arithmetic Operators.c

---
+++ Arithmetic Operators.c
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    c=a+b
+    print(c)
+
+    c=a-b                      
+    print(c)
+
+    c=a*b                       
+    print(c)
